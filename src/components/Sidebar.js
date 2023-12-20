c84a7444436e61a6a66a178ce55c77bac6fa7cc8
matlin <cymatlin@gmail.com>
2023-12-20 20:19:34 +0200

    add style for sidbar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,8 +1,21 @@
-
-import React from 'react';
+// src/components/Sidebar.js
+import React, { useState } from 'react';
+import './Sidebar.css'; // Assuming you have a separate CSS file for Sidebar
 
 function Sidebar() {
-  return <header>Sidebar</header>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+      <button className="toggle-button" onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? 'Close' : 'Open'}
+      </button>
+      <div className="sidebar-content">
+        {/* Sidebar content goes here */}
+        <p>Sidebar</p>
+      </div>
+    </div>
+  );
 }
 
 export default Sidebar;
